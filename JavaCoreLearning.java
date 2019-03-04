91cb84cc28341e1b0a9a5569a80cab073a863cbf
lzyyxtk <lzyyxtk@gmail.com>
2019-03-03 16:30:22 -0800

    Playground and learning notes added

---
+++ JavaCoreLearning.java
@@ -0,0 +1,5 @@
+public class JavaCoreLearning {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ LearningNote.java
@@ -0,0 +1,5 @@
+public class LearningNote {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
